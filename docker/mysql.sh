2c6b4154d7f38ef17b7631ee4128a88eba4fac50
mark <mayong_82@126.com>
2023-04-03 23:19:21 +0800

    Update mysql.sh

--- docker/mysql.sh
+++ docker/mysql.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
 docker rm -f mysql5.7
 docker run --name mysql5.7 -p 3306:3306 \
-   -e MYSQL_ROOT_PASSWORD=Legend2023 \
+   -e MYSQL_ROOT_PASSWORD=123456 \
    -v /docker_data/mysql/data:/var/lib/mysql \
    -v /docker_data/mysql/conf:/etc/mysql/ \
    -v /docker_data/mysql/logs:/var/log/mysql  \
    -d mysql:5.7
+
+
