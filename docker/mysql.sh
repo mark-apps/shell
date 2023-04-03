d752f7f675031567d6f4c6e24751ac4b95010295
mark <mayong_82@126.com>
2023-04-03 23:18:14 +0800

    Create mysql.sh

---
+++ docker/mysql.sh
@@ -0,0 +1,7 @@
+docker rm -f mysql5.7
+docker run --name mysql5.7 -p 3306:3306 \
+   -e MYSQL_ROOT_PASSWORD=Legend2023 \
+   -v /docker_data/mysql/data:/var/lib/mysql \
+   -v /docker_data/mysql/conf:/etc/mysql/ \
+   -v /docker_data/mysql/logs:/var/log/mysql  \
+   -d mysql:5.7
