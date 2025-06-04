9dc1819b09b5c80ff077eb710e3208f9f6ddbe21
mark <mark@legend.tech>
2025-06-04 16:33:37 +0800

    Update file

--- bhapp/getByUserId.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-userId=$1
-curl "https://m.cbhb.com.cn/mbank/public/ecif.MCPerQryCifinfoByUserID.do" \
- -H 'Content-type:application/json' \
- --data "{\"UserId\":\"$userId\",\"MChannel\":\"PMBS\"}"|jq .
